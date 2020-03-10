b0df7a8cbd70b9dc364b31663b70a96aaaac0b53
Alex Titov <alextitov203@gmail.com>
2020-03-10 09:23:33 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'SwiftPortfolio' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftPortfolio
+
+pod'AWSCore'
+pod'AWSDynamoDB'
+
+end
---
+++ SwiftPortfolio/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftPortfolio
+//
+//  Created by Titov, Aleksandr on 3/9/20.
+//  Copyright © 2020 Titov, Aleksandr. All rights reserved.
+//
+
+import UIKit
+import AWSDynamoDB
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
